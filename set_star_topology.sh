487c1741c1fa798cb861f0893c969b4a1ad80bef
Giuseppe Maxia <g.maxia@gmail.com>
2015-08-13 06:59:58 +0200

    Added support for star topology in mariadb

--- set_star_topology.sh
+++ set_star_topology.sh
@@ -8,31 +8,79 @@
 # Changes from the endpoints are replicated to the hub,
 # and from there to the other endpoints.
 
+VERSION=$1
+FLAVOR=$2
+
+case $FLAVOR in
+    mysql)
+        ;;
+    mariadb)
+        ;;
+    *)
+        unset VERSION
+        ;;
+esac
+
+if [ -z "$VERSION" ]
+then
+    echo "VERSION and FLAVOR required"
+    echo "Where VERSION is an indentifier like 5.7.7 or ma10.0.20 "
+    echo "      and FLAVOR is either mysql or mariadb"
+   exit 1 
+fi
+
 . ./common.sh
 
-make_multiple_sandbox --how_many_nodes=5 5.7.8
+[ -z "$SANDBOX_BINARY" ] && SANDBOX_BINARY=$HOME/opt/mysql
 
-cd ~/sandboxes/multi_msb_5_7_8
+if [ ! -d $SANDBOX_BINARY/$VERSION ]
+then
+    echo "$SANDBOX_BINARY does not exist"
+    echo "Set the variable SANDBOX_BINARY to indicate where to find the expanded tarballs for MySQL::Sandbox"
+    exit 1
+fi
 
-for NODE in  node1 node2 node3 node4 node5
-do
-    ./$NODE/use -e "create table test.not_replicated_$NODE(id int)"
-done
+DASHED_VERSION=$(echo $VERSION| tr '.' '_')
+sandbox_name=$HOME/sandboxes/multi_msb_$DASHED_VERSION
+
+make_multiple_sandbox --how_many_nodes=5 $VERSION
+
+cd $sandbox_name
+./use_all 'reset master'
+
+#for NODE in  node1 node2 node3 node4 node5
+#do
+#    ./$NODE/use -e "create table test.not_replicated_$NODE(id int)"
+#done
 
 OPTIONS="master-info-repository=table "
 OPTIONS="$OPTIONS relay-log-info-repository=table"
 OPTIONS="$OPTIONS gtid_mode=ON"
 OPTIONS="$OPTIONS enforce-gtid-consistency"
+if [ "$FLAVOR" == "mariadb" ]
+then
+    OPTIONS=""
+fi
+
 HUB_OPTIONS="$OPTIONS log-slave-updates "
-CHANGED=""
 HUB=node3
+NODE_COUNT=0
 for NODE in  node1 node2 node3 node4 node5
 do
+    CHANGED=""
+    NODE_COUNT=$(($NODE_COUNT+1))
     LOCAL_OPTIONS=$OPTIONS
     if [ $NODE == $HUB ]
     then
         LOCAL_OPTIONS=$HUB_OPTIONS
     fi
+    if [ "$FLAVOR" == "mariadb" ]
+    then
+        SERVER_ID=$($NODE/use -BN -e 'select @@server_id')
+        DOMAIN=$(($SERVER_ID*10))
+        LOCAL_OPTIONS="$LOCAL_OPTIONS gtid_domain_id=$DOMAIN"
+    fi
+
     for OPTION in $LOCAL_OPTIONS
     do
         option_exists=$(grep $OPTION $NODE/my.sandbox.cnf)
@@ -45,11 +93,11 @@ do
             echo "# option '$OPTION' already exists in $NODE configuration file"
         fi
     done
+    if [ -n "$CHANGED" ]
+    then
+        $NODE/restart
+    fi
 done
-if [ -n "$CHANGED" ]
-then
-    ./restart_all
-fi
 
 HUB=node3
 ENDPOINTS=(node1 node2 node4 node5)
@@ -62,24 +110,40 @@ for NODE in ${ENDPOINTS[*]}
 do 
     NODE_PORT=$(get_port ./$NODE/use)
     echo "# node $NODE port: $NODE_PORT" 
-    CHANGE_MASTER_FIXED="CHANGE MASTER TO master_host='127.0.0.1', master_user='rsandbox', master_password='rsandbox'" 
-    #node_file_and_pos=$(get_change_master_file_and_pos $NODE/use)
-    #hub_file_and_pos=$(get_change_master_file_and_pos $HUB/use)
-    #CHANGE_MASTER_HUB_TO_NODE=", master_port=$NODE_PORT, $hub_file_and_pos for channel '${HUB}_${NODE}'"
-    #CHANGE_MASTER_NODE_TO_HUB=", master_port=$HUB_PORT, $node_file_and_pos for channel '${NODE}_${HUB}'"
-    CHANGE_MASTER_NODE_TO_HUB=", master_port=$HUB_PORT, MASTER_AUTO_POSITION=1 for channel '${NODE}_${HUB}'"
-    CHANGE_MASTER_HUB_TO_NODE=", master_port=$NODE_PORT, MASTER_AUTO_POSITION=1 for channel '${HUB}_${NODE}'"
-    echo "./$NODE/use -e <$CHANGE_MASTER_FIXED $CHANGE_MASTER_NODE_TO_HUB>"
-    ./$NODE/use -e "$CHANGE_MASTER_FIXED $CHANGE_MASTER_NODE_TO_HUB"
+    NODE_CHANNEL=${NODE}_${HUB}
+    HUB_CHANNEL=${HUB}_${NODE}
+    CHANGE_MASTER_FIXED="master_host='127.0.0.1', master_user='rsandbox', master_password='rsandbox'" 
+    case $FLAVOR in 
+        mariadb)
+            auto_position="MASTER_USE_GTID=current_pos"
+            CHANGE_MASTER_NODE_TO_HUB="CHANGE MASTER '$NODE_CHANNEL' TO $CHANGE_MASTER_FIXED, master_port=$HUB_PORT, $auto_position "
+            CHANGE_MASTER_HUB_TO_NODE="CHANGE MASTER '$HUB_CHANNEL' TO $CHANGE_MASTER_FIXED, master_port=$NODE_PORT, $auto_position "
+            START_SLAVE_NODE="START SLAVE '$NODE_CHANNEL' "
+            START_SLAVE_HUB="START SLAVE '$HUB_CHANNEL' "
+            ;;
+        mysql)
+            auto_position="MASTER_AUTO_POSITION=1"
+            CHANGE_MASTER_NODE_TO_HUB="CHANGE MASTER TO $CHANGE_MASTER_FIXED, master_port=$HUB_PORT, $auto_position for channel '$NODE_CHANNEL'"
+            CHANGE_MASTER_HUB_TO_NODE="CHANGE MASTER TO $CHANGE_MASTER_FIXED, master_port=$NODE_PORT, $auto_position for channel '$HUB_CHANNEL'"
+            START_SLAVE_NODE="START SLAVE FOR CHANNEL '$NODE_CHANNEL'"
+            START_SLAVE_HUB="START SLAVE FOR CHANNEL '$HUB_CHANNEL'"
+        ;;
+        *)
+            echo "unexpected flavor <$FLAVOR>"
+            exit 1
+            ;;
+    esac
+    echo "./$NODE/use -e <$CHANGE_MASTER_NODE_TO_HUB>"
+    ./$NODE/use -e "$CHANGE_MASTER_NODE_TO_HUB"
 
-    echo "./$HUB/use -e <$CHANGE_MASTER_FIXED $CHANGE_MASTER_HUB_TO_NODE>"
-    ./$HUB/use -e "$CHANGE_MASTER_FIXED $CHANGE_MASTER_HUB_TO_NODE"
+    echo "./$HUB/use -e <$CHANGE_MASTER_HUB_TO_NODE>"
+    ./$HUB/use -e "$CHANGE_MASTER_HUB_TO_NODE"
 
-    echo "./$HUB/use -e <start slave for channel '${HUB}_${NODE}'>"
-    ./$HUB/use -e "start slave for channel '${HUB}_${NODE}'"
+    echo "./$HUB/use -e <$START_SLAVE_HUB>"
+    ./$HUB/use -e "$START_SLAVE_HUB"
 
-    echo "./$NODE/use -e <start slave for channel '${NODE}_${HUB}'>"
-    ./$NODE/use -e "start slave for channel '${NODE}_${HUB}'"
+    echo "./$NODE/use -e <$START_SLAVE_NODE>"
+    ./$NODE/use -e "$START_SLAVE_NODE"
 done
 
 for NODE in  node1 node2 node3 node4 node5
