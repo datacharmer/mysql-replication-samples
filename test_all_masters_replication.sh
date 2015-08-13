84848b9e39bb014dfb14572f27949eac2f1a3843
Giuseppe Maxia <g.maxia@gmail.com>
2015-08-13 07:17:32 +0200

    test is now capable of running with up to 9 nodes

--- test_all_masters_replication.sh
+++ test_all_masters_replication.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 cd $(dirname $0)
 
-for NODE in node1 node2 node3 node4
+for N in $(seq 1 9)
+do
+    if [ -d node$N ]
+    then
+        NODES[$N]=node$N
+    fi
+done
+
+for NODE in ${NODES[*]}
 do
     $NODE/use test -e "drop table if exists test_$NODE"
     $NODE/use test -e "create table test_$NODE( id int not null primary key, serverid int, dbport int, node varchar(100), ts timestamp)"
     $NODE/use test -e "insert into test_$NODE values (1, @@server_id, @@port, '$NODE', null)"
-    # echo -n "# Tables in server "
-    # $NODE/use -BN -e 'select @@server_id; show tables from test'
     echo "# NODE $NODE created table test_$NODE"
 done
 
 sleep 3
 echo "# Data in all nodes"
-for NODE in node1 node2 node3 node4
+for NODE in ${NODES[*]}
 do
     $NODE/use -BN -e 'select @@server_id'
-    for TABLE_NAME in node1 node2 node3 node4
+    for TABLE_NAME in ${NODES[*]}
     do
         $NODE/use test -BN -e "select * from test_$TABLE_NAME"
     done
