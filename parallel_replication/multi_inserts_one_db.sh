98a850d153f89b70ad4cbcb272313b94473781cf
Giuseppe Maxia <g.maxia@gmail.com>
2015-08-23 18:34:15 +0200

    Fixed small issue in table name creation

--- parallel_replication/multi_inserts_one_db.sh
+++ parallel_replication/multi_inserts_one_db.sh
@@ -9,6 +9,6 @@ for D in $(seq 1 10)
 do
     for N in $(seq 1 10)
     do
-        ./insert_many.sh ./m test ${D}_t$N 500 > ./tmp/out_db${D}_t$N.txt 2>&1 &
+        ./insert_many.sh ./m test d${D}_t$N 500 > ./tmp/out_d${D}_t$N.txt 2>&1 &
     done 
 done
