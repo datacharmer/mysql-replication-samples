41196cb023b3fe109682bdbbf5d0cf322a96453b
Giuseppe Maxia <g.maxia@gmail.com>
2015-10-18 17:50:18 +0200

    Added removal of shortcut files

--- docker-replication/remove-nodes.sh
+++ docker-replication/remove-nodes.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
-NUM_NODES=$1
-[ -z "$NUM_NODES" ] && NUM_NODES=3
 
-set -x
+if [ -f DEPLOYED ]
+then
+    NUM_NODES=$(cat DEPLOYED)
+else
+    echo "#File 'DEPLOYED' not found. Assuming 3 nodes"
+    NUM_NODES=3
+fi
+
+# set -x
 for NODE in $(seq 1 $NUM_NODES)
 do
     docker stop mysql-node$NODE
     docker rm mysql-node$NODE
+    SN=$(($NODE-1))
+    if [ -x n$NODE ]
+    then
+        rm -f n$NODE
+    fi
+    if [ -L s$SN ]
+    then
+        rm -f s$SN
+    fi
 done
+if [ -L m ]
+then
+    rm -f m
+fi
+rm -f DEPLOYED
