9da0ed4aed3a76ef3d0f85bd31cd7af62ec82d84
Giuseppe Maxia <g.maxia@gmail.com>
2015-10-25 10:10:34 -0700

    Cleaned up and simplified deploy-nodes.sh

--- docker-replication/deploy-nodes.sh
+++ docker-replication/deploy-nodes.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 [ -z "$MIN_DOCKER_VERSION" ] && export MIN_DOCKER_VERSION=1.7.0
+curdir=$(dirname $0)
 
-. ./common.sh
+. $curdir/common.sh
+[ -z "$MYSQL_IMAGE" ] && MYSQL_IMAGE=mysql/mysql-server
 
 check_docker_version
+check_operating_system
 
 NUM_NODES=$1
 if [ -z "$NUM_NODES" ]
@@ -22,38 +25,49 @@ for NODE in $( seq 1 $NUM_NODES )
 do
     export NODE
     echo "# $NODE"
-    if [ -f /tmp/my_$NODE.cnf ]
+    if [ -f $DOCKER_TMP/my_$NODE.cnf ]
     then
-        sudo rm /tmp/my_$NODE.cnf
+        sudo rm $DOCKER_TMP/my_$NODE.cnf
     fi
-    if [ -f /tmp/home_my_$NODE.cnf ]
+    if [ -f $DOCKER_TMP/home_my_$NODE.cnf ]
     then
-        sudo rm /tmp/home_my_$NODE.cnf
+        sudo rm $DOCKER_TMP/home_my_$NODE.cnf
     fi
-    sed "s/_SERVERID_/${NODE}00/" < my-template.cnf > /tmp/my_$NODE.cnf
-    cp home_my.cnf /tmp/home_my_$NODE.cnf
-    echo "[mysql]" >> /tmp/home_my_$NODE.cnf
-    # echo "prompt=node$NODE >> " >> /tmp/home_my_$NODE.cnf
-    echo "prompt='node$NODE [\\h] {\\u} (\\d) > '" >> /tmp/home_my_$NODE.cnf
-    if [ ! -d /opt/docker/mysql ]
+    sed "s/_SERVERID_/${NODE}00/" < my-template.cnf > $DOCKER_TMP/my_$NODE.cnf
+    cp home_my.cnf $DOCKER_TMP/home_my_$NODE.cnf
+    echo "[mysql]" >> $DOCKER_TMP/home_my_$NODE.cnf
+    # echo "prompt=node$NODE >> " >> $DOCKER_TMP/home_my_$NODE.cnf
+    if [ "$NODE" == "1" ]
     then
-        sudo mkdir -p /opt/docker/mysql
-        sudo chown -R mysql /opt/docker/mysql
-        sudo chgrp -R mysql /opt/docker/mysql
+        NAME=master
+    else
+        NAME="node$NODE"
     fi
-    if [ -d /opt/docker/mysql/node_$NODE ]
+    echo "prompt='$NAME [\\h] {\\u} (\\d) > '" >> $DOCKER_TMP/home_my_$NODE.cnf
+    if [ "$DATA_VOLUME" == "YES" ]
     then
-        sudo rm -rf /opt/docker/mysql/node_$NODE
+        if [ ! -d $DOCKER_DATA ]
+        then
+            sudo mkdir -p $DOCKER_DATA
+            sudo chown -R mysql $DOCKER_DATA
+            sudo chgrp -R mysql $DOCKER_DATA
+        fi
+        if [ -d $DOCKER_DATA/node_$NODE ]
+        then
+            sudo rm -rf $DOCKER_DATA/node_$NODE
+        fi
+        DATA_OPTION="-v $DOCKER_DATA/node_$NODE:/var/lib/mysql"
+    else
+        DATA_OPTION=""
     fi
-    # exit
-    # cat /tmp/my_$NODE.cnf
     echo ""
+    echo "# Deploying $MYSQL_IMAGE into container mysql-node$NODE"
     docker run --name mysql-node$NODE  \
-        -v /tmp/my_$NODE.cnf:/etc/my.cnf \
-        -v /tmp/home_my_$NODE.cnf:/root/home_my.cnf \
-        -v /opt/docker/mysql/node_$NODE:/var/lib/mysql \
-        -e MYSQL_ROOT_PASSWORD=secret \
-        -d mysql:5.7.8-rc
+        -v $DOCKER_TMP/my_$NODE.cnf:/etc/my_second.cnf \
+        -v $DOCKER_TMP/home_my_$NODE.cnf:/root/home_my.cnf \
+        -e MYSQL_ROOT_PASSWORD=secret $DATA_OPTION \
+        -d $MYSQL_IMAGE
+
     if [ "$?" != "0" ] ; then exit 1; fi
 done
 
@@ -86,6 +100,7 @@ do
     MAX_ATTEMPTS=30
     ATTEMPTS=0
     node_ready=''
+    echo "# Checking container mysql-node$NODE"
     while [ "$node_ready" != "OK" ]
     do
         ATTEMPTS=$(($ATTEMPTS+1))
@@ -118,7 +133,10 @@ do
     #
     # Set username and password in private file
     # Notice that this operation cannot happen before MySQL initialization
+    echo "# Reconfiguring server in mysql-node$NODE"
     docker exec -it mysql-node$NODE cp /root/home_my.cnf /root/.my.cnf
+    docker exec -it mysql-node$NODE cp /etc/my_second.cnf /etc/my.cnf
+    docker restart mysql-node$NODE 
 done
 
 if [ -n "$SKIP_REPLICATION" ]
