d38a419723d2982632dee366994715efbf056855
Giuseppe Maxia <g.maxia@gmail.com>
2016-02-14 15:31:08 +1100

    Added example scripts for percona server 5.6

---
+++ docker-replication/start_percona.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# example of using experimental Percona image with Tokudb
+echo never > never.txt
+docker run \
+    --name mybox  \
+    -e MYSQL_ROOT_PASSWORD=secret -d \
+    -e INIT_TOKUDB=1 \
+    -v $PWD/never.txt:/sys/kernel/mm/transparent_hugepage/defrag \
+    -v $PWD/never.txt:/sys/kernel/mm/transparent_hugepage/enabled \
+    percona/percona-server
+
+
