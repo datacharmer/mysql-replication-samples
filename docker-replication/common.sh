9a519378819d7d33383012967d418337473a0c71
Giuseppe Maxia <g.maxia@gmail.com>
2016-02-21 17:32:14 +0100

    Fixed mistake in version comparison
    When the version minor jumps to double digit, the comparison fails.
    The new method normalizes the version numbers before running the
    comparison.

--- docker-replication/common.sh
+++ docker-replication/common.sh
@@ -16,20 +16,36 @@ function check_operating_system
     fi
 }
 
+function normalized_version
+{
+    v=$1
+    PARTS=$(echo $v | tr '.' ' ' | wc -w)
+    if [[ $PARTS -lt 3 ]]
+    then
+        echo "# Version '$v' should have 3 components. Found only $PARTS"
+        exit 1
+    fi
+    V1=$(echo $v | tr '.' ' ' | awk '{print $1}')
+    V2=$(echo $v | tr '.' ' ' | awk '{print $2}')
+    V3=$(echo $v | tr '.' ' ' | awk '{print $3}')
+    printf "%02d.%02d.%02d" $V1 $V2 $V3
+}
 
 function check_docker_version ()
 {
     [ -z "$MIN_DOCKER_VERSION" ] && MIN_DOCKER_VERSION=1.7.0
     DOCKER_VERSION=$(docker --version | perl -lne 'print $1 if /(\d+\.\d+\.\d+)/')
+    DOCKER_NORMALIZED_VERSION=$(normalized_version $DOCKER_VERSION)
+    MIN_DOCKER_NORMALIZED_VERSION=$(normalized_version $MIN_DOCKER_VERSION)
 
     # To check if the current version matches the requirement, we sort numerically both
     # the required version and the current one. Then we get the top one.
     # If top version that results from sorting is the current docker version, then
     # the check passes. If not, we need to upgrade docker
-    MAX_VERSION=$((echo $MIN_DOCKER_VERSION ; echo $DOCKER_VERSION ) | sort -nr| head -1 )
+    MAX_VERSION=$((echo $MIN_DOCKER_NORMALIZED_VERSION ; echo $DOCKER_NORMALIZED_VERSION ) | sort -nr| head -1 )
 
     echo -n "# Docker version "
-    if [ "$MAX_VERSION" == "$DOCKER_VERSION" ]
+    if [ "$MAX_VERSION" == "$DOCKER_NORMALIZED_VERSION" ]
     then
         echo ok
     else
